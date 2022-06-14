074433a7ea9577f9e38923db4b639c35b449ae4c
Mason Mung <mason5991@yahoo.com.hk>
2022-06-14 14:14:53 +0800

    init

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "location" {
+    description = "Location of the network"
+    default     = "eastus"
+}
+
+variable "username" {
+    description = "Username for Virtual Machines"
+    default     = "azureuser"
+}
+
+variable "password" {
+    description = "Password for Virtual Machines"
+}
+
+variable "vmsize" {
+    description = "Size of the VMs"
+    default     = "Standard_DS1_v2"
+}
+
+locals {
+  timeouts = {
+    create = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+}
